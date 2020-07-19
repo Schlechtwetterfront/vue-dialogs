6de1fa57709a1a2a23d346d22d6d86f402e1b92b
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2020-07-19 17:08:06 +0200

    Improve playground styling

--- examples/components/App.vue
+++ examples/components/App.vue
@@ -32,6 +32,17 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+body {
+    color: #333;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+        'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+* {
+    box-sizing: border-box;
+}
+
 .playground {
     height: 100%;
 
